299490fd1315c9c26daa199e523121a8256f6392
Art Goldhammer <art.goldhammer@gmail.com>
2022-02-21 20:29:02 -0500

    preparing to serve doc

--- src/agold/query.cljs
+++ src/agold/query.cljs
@@ -2,7 +2,12 @@
   (:require [cljs-http.client :as http]
             [cljs.core.async :as a]
             [cljs.pprint :as pp]
+            [process]
+            ["fs" :as fs]
             ["xmlhttprequest" :refer [XMLHttpRequest]]))
+
+(process/on "uncaughtException", (fn [err origin]
+                              (println "Uncaught Exception" err origin)))
 
 ;; for this hack, needed to make cljs-http work properly
 ;; see http://www.jimlynchcodes.com/blog/solved-xmlhttprequest-is-not-defined-with-clojurescript-coreasync
@@ -11,6 +16,12 @@
 
 ;; this eliminates the annoying message from xhr2, which is no longer required
 (set! js/XMLHttpRequest XMLHttpRequest)
+
+(defn slurp
+  "read file into string"
+  [fname]
+  (-> (fs/readFileSync fname)
+      (.toString)))
 
 
 (def base-url "http://localhost:5000")
@@ -28,6 +39,41 @@
                                    {:with-credentials? false
                                     :query-params query-param-map}))]
       (println (:body response))))
+  :fetched)
+
+(def out (atom ""))
+(defn graphout
+  [result]
+  #_(reset! out (clj->js (stringify-keys (:result result))))
+  (println (.stringify js/JSON (clj->js (dissoc (:result result) :time))))
+
+  #_(reset! out result))
+
+(def graph-route {
+                  :endpoint "/json/xgraph"
+                  :json-params {:subqueries [["Pecresse"] ["Zemmour"] ["Pen"]] :start "2022-02-11"
+                             :interval "1d" :n 10}
+                  :ok-fn graphout
+                  :err-fn println
+})
+
+(defn post-endpoint-x
+  "fetch data from route map"
+  [route]
+  (let
+   [{:keys [endpoint json-params ok-fn err-fn]} route]
+    (a/go
+      (let [response (a/<! (http/post (make-url endpoint)
+                                      {:with-credentials? false
+                                       :json-params json-params}))
+            status (:status response)]
+        (if (= status 200)
+          (let [body (:body response)
+                err (:error body)]
+            (if (= err 0)
+              (ok-fn body)
+              (err-fn "fetch error:" err)))
+          (err-fn "network error")))))
   :fetched)
 
 (defn post-endpoint
@@ -58,7 +104,15 @@
          (post-endpoint "/json/xcount" {:words ["Putin", "Poutine", "Ukraine", "Zelensky"] :start "2022-02-15"
                                         :end "2022-02-16"})
          (post-endpoint "/json/xcount" {:words ["Scholz"] :start "2022-02-15"
-                                        :end "2022-02-16"}))
+                                        :end "2022-02-16"})
+         (post-endpoint "/json/xgraph" {:subqueries [["Scholz"]["Macron"]["Putin"]] :start "2022-02-15"
+                                        :interval "1d" :n 7})
+         (post-endpoint-x graph-route)
+         @out
+         (println (.stringify js/JSON (clj->js (dissoc (:result @out) :time))))
+         (def index-templ (slurp "resources/index.templ"))
+         (.replace index-templ "VGJSON" "xyz")
+         )
 
 
 
